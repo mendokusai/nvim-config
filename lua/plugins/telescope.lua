80abc8e9707aecda3b7f596a6d10e1f883d428b0
ryan <ryanpauley@gmail.com>
2025-06-24 21:12:39 +1000

    Steals sean's telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>t", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Find buffers" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>fG", builtin.git_status, { desc = "Git status" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help" })
+      vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Find symbols in buffer" })
+      vim.keymap.set("n", "<leader>fS", bu[48;46;110;1380;1540tiltin.lsp_dynamic_workspace_symbols, { desc = "Find symbols in workspace" })
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({
+              -- even more opts
+            }),
+          },
+        },
+      })
+      -- To get ui-select loaded and working with telescope, you need to call
+      -- load_extension, somewhere after setup function:
+      require("telescope").load_extension("ui-select")
+      require("telescope").load_extension("noice")
+    end,
+  },
+}
