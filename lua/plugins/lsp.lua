848917a66efbacb199472f8f0ffb898fff177fda
ryan <ryanpauley@gmail.com>
2025-11-10 10:34:03 +1100

    Checks' solargraph's ruby dependency against asdf's instance

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,14 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require("lspconfig").solargraph.setup({})
+      local solargraph_path = vim.fn.system("asdf which solargraph | tr -d '\n'")
+      if vim.v.shell_error == 0 then
+        require("lspconfig").solargraph.setup({
+          cmd = { solargraph_path, "stdio" },
+        })
+      else
+        require("lspconfig").solargraph.setup({})
+      end
     end,
   },
 }
