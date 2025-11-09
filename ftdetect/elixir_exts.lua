ac2c38ed020efca95e373cba4b3f241dd1226dcf
ryan <ryanpauley@gmail.com>
2025-11-10 10:43:43 +1100

    Was mixing ex & exs from Elixir syntax checks, which is weird

--- ftdetect/elixir_exts.lua
+++ ftdetect/elixir_exts.lua
@@ -1,9 +1,10 @@
--- Match Elixir filetypes as elixir.
-vim.filetype.add {
+vim.filetype.add({
   extension = {
+    ex = "elixir",
+    exs = "elixir",
     heex = "heex",
     leex = "leex",
     sface = "sface",
     livemd = "livemd",
   },
-}
+})
